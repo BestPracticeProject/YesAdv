c41e2f178e61fe988e4ad312e4561b7b309d27b9
IegorKozakov <kozakov.e@gmail.com>
2014-06-09 18:23:55 +0300

    Navigation testing

--- resources/common/js/common.js
+++ resources/common/js/common.js
@@ -3,6 +3,27 @@
 
 	/* trigger when page is ready */
 	$(function(){
+		var $wrapper = $(document).find('.j-wrapper'),
+				$nav = $wrapper.find('.j-nav'),
+				$navBtn = $nav.find('.j-nav_btn'),
+				$navList = $nav.find('.j-nav_list');
+
+		/* MENU */
+		$navBtn.on('click', function(){
+			var that = $(this);
+
+			if($nav.hasClass('nav-active')){
+				$nav.removeClass('nav-active');
+			} else {
+				$nav.addClass('nav-active');
+			}
+		});
+
+
+		/* slider */
+  	$('.bxslider').bxSlider({
+  		pager: false
+  	});
 
 	});
 
@@ -16,3 +37,4 @@
 	});
 	*/
 }(window.jQuery));
+
