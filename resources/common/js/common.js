cd7a1ab6fd3ab0617ac046f810c0bdbf72338651
dima <dspwww@gmail.com>
2014-06-15 18:50:04 +0400

    some js

--- resources/common/js/common.js
+++ resources/common/js/common.js
@@ -6,12 +6,13 @@
 		var $wrapper = $(document).find('.j-wrapper'),
 				$nav = $wrapper.find('.j-nav'),
 				$navBtn = $nav.find('.j-nav_btn'),
-				$navList = $nav.find('.j-nav_list');
+				$navList = $nav.find('.j-nav_list'),
+				$works = $wrapper.find('.j-works'),
+				$worksCategories = $works.find('.j-w_category'),
+				$worksExample = $works.find('.j-w_examples');
 
 		/* MENU */
 		$navBtn.on('click', function(){
-			var that = $(this);
-
 			if($nav.hasClass('nav-active')){
 				$nav.removeClass('nav-active');
 			} else {
@@ -19,11 +20,37 @@
 			}
 		});
 
-
 		/* slider */
-  	$('.bxslider').bxSlider({
-  		pager: false
-  	});
+		$('.bxslider').bxSlider({
+			pager: false
+		});
+
+		/* works */
+		$worksCategories.on('click', '.j-cat_btn', function(){
+			var that = $(this);
+			if(!that.hasClass('m-wtypes__btn-active')){
+				var category = that.attr("href"),
+					$listWorks = $worksExample.find('.j-work'),
+					amountOfWorks = $listWorks.length,
+					$reserv = $works.find('.j-works_reserv');
+
+					$.each($listWorks, function(index, val) {
+						var work = $(this);
+
+						if(work.data('cat') != category){
+							var i = work.addClass('witem-hidden').detach();
+							i.appendTo($reserv);
+						} else {
+							work.removeClass('witem-hidden');
+						}
+					});
+
+
+
+			};
+			return false;
+		});
+
 
 	});
 
